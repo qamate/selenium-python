d754ded43d0b98681173ece13ea81e878a2a9c2c
Mayur <mayur@kagrana.com>
2014-11-27 09:08:07 +0530

    modified code

--- example.py
+++ example.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
-driver.get("http://www.kagrana.com")
 driver.maximize_window()
+driver.get("http://www.kagrana.com")
 assert "Home - Kagrana Software Pvt. Ltd. | Kagrana Software Pvt. Ltd." in driver.title
 elem = driver.find_element_by_name("s")
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+assert "Nothing Found" in driver.page_source
 driver.close()
